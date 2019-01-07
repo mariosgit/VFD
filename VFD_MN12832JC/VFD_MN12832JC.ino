1f63b2cfaed0e4f3a9c99fd2edcc10fb0b52b516
Mario Becker <mariosge@gmail.com>
2019-01-07 15:19:19 +0100

    Added some drawing demo code.

--- VFD_MN12832JC/VFD_MN12832JC.ino
+++ VFD_MN12832JC/VFD_MN12832JC.ino
@@ -3,11 +3,14 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <mbLog.h>
+#include <elapsedMillis.h>
 
 #include "mbGFX_MN12832JC.h"
 
 // useable space is 126 width, one coloum missing at the sides
 MN12832JC canvas;  //  uint8_t *getBuffer(void);
+elapsedMillis frameTime;
+elapsedMillis textChangeTime;
 
 void drawSomething()
 {
@@ -62,25 +65,50 @@ int i = 0;
 int dir = 1;
 void loop()
 {
-    // // fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
-    canvas.fillRect(36,0,127,23,0);
-    canvas.setCursor(36+i, 1+random(3));
-    canvas.setTextSize(2);
-    canvas.print("MAKER");
-    if(i == 0)
-        dir = 1;
-    if(i == 32)
-        dir = -1;
-    i = (i + dir);
+    byte xpos = 36;
+    char* tmaker = "MAKER";
+    if(textChangeTime > 1000)
+    {
+        xpos = 50;
+        tmaker = "was";
+    }
+    if(textChangeTime > 2000)
+    {
+        xpos = 36;
+        tmaker = "here!";
+    }
+    if(textChangeTime > 3000)
+    {
+        xpos = 48;
+        tmaker = "8-P";
+    }
+    if(textChangeTime > 4000)
+    {
+        textChangeTime = 0;
+    }
+    if(frameTime > 20) // draw stuff with 50Hz
+    {
+        frameTime = 0;
+        // // fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
+        canvas.fillRect(36,0,127,23,0);
+        canvas.setCursor(xpos+i, 1+random(3));
+        canvas.setTextSize(2);
+        canvas.print(tmaker);
+        if(i == 0)
+            dir = 1;
+        if(i == 32)
+            dir = -1;
+        i = (i + dir);
 
-    int i12 = (i) / 14 ;
-    canvas.fillCircle(7,10,7,1);
-    canvas.fillCircle(22,10,7,1);
-    canvas.fillCircle(7+i12,9+random(3),3,0);
-    canvas.fillCircle(22-i12,9+random(3),3,0);
-    canvas.drawLine(14,19,15,19,1);
+        int i12 = (i) / 14 ;
+        canvas.fillCircle(7,10,7,1);
+        canvas.fillCircle(22,10,7,1);
+        canvas.fillCircle(7+i12,9+random(3),3,0);
+        canvas.fillCircle(22-i12,9+random(3),3,0);
+        canvas.drawLine(14,19,15,19,1);
 
-    canvas.swapBuffers();
+        canvas.swapBuffers();
 
-    LOG <<"disp time:" <<canvas.getDisplayTime() <<"us\n";
+        LOG <<"disp time:" <<canvas.getDisplayTime() <<"us\n";
+    }
 }
