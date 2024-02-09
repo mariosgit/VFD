7e32eb7886ef6b9f9bed20615ae8a895a6adf1db
Mario Becker <mariosge@gmail.com>
2024-02-09 19:43:09 +0100

    Added test code for GP1294

--- GP1294-software/GP1294Controller/src/main.cpp
+++ GP1294-software/GP1294Controller/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <U8g2lib.h>
+#include <SPI.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+U8G2_GP1294AI_256X48_F_4W_HW_SPI u8g2(U8G2_R0, 10, U8X8_PIN_NONE, 9);
+
+void setup()
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    u8g2.begin();
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(10, 24, "Hello World!");
+    u8g2.drawLine(0, 0, 64, 64);
+    u8g2.drawLine(64, 0, 2*64, 64);
+    u8g2.drawLine(2*64, 0, 3*64, 64);
+    u8g2.sendBuffer();
+    delay(10);
 }
--- GP1294-software/attiny-input/attiny-input.ino
+++ GP1294-software/attiny-input/attiny-input.ino
@@ -12,61 +12,85 @@
 
 const byte SLAVE_ADDR = 100;
 const byte NUM_BYTES = 4;
-volatile int8_t data[NUM_BYTES] = { 0, 1, 2, 3 };
+volatile int8_t data[NUM_BYTES] = {0, 1, 2, 3};
 
 ClickEncoder encoder(0, 1, 2, 4); // alps needs the 4 step stuff
-TM1637 tm1637(10, 9); // clk/dio
+TM1637 tm1637(10, 9);             // clk/dio
 
-void setup() {
-  TinyWireS.begin(SLAVE_ADDR);
-  TinyWireS.onRequest(OnTinyWireSRequest);
+const byte pinLatch = 3;
+const byte pinSCLK = 10;
+const byte pinMISO = 9;
 
-  tm1637.init();
-  tm1637.set(BRIGHT_DARKEST);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+void setup()
+{
+    TinyWireS.begin(SLAVE_ADDR);
+    TinyWireS.onRequest(OnTinyWireSRequest);
 
-//  encoder.setDoubleClickEnabled(false); // dann gibts auch kein click !
-  SPI.begin();
+    tm1637.init();
+    tm1637.set(BRIGHT_DARKEST); // BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+
+    //  encoder.setDoubleClickEnabled(false); // dann gibts auch kein click !
+    // SPI.begin();
+    // SPI.setDataMode(SPI_MODE2);
+
+    pinMode(pinLatch, OUTPUT);
+    pinMode(pinSCLK, OUTPUT);
+    pinMode(pinMISO, INPUT);
 }
-
 
 int counter = 0;
-void loop() {
-  encoder.service();
+void loop()
+{
+    encoder.service();
 
-  data[0] += encoder.getValue();
-  ClickEncoder::Button b = encoder.getButton();
-  if(b == ClickEncoder::Clicked)
-    data[1] += 1;
-  if(b == ClickEncoder::Released) // kommt nur nach Held
-    data[1] += 0;
-  if(b == ClickEncoder::Held)
+    data[0] += encoder.getValue();
+    ClickEncoder::Button b = encoder.getButton();
+    if (b == ClickEncoder::Clicked)
+        data[1] += 1;
+    if (b == ClickEncoder::Released) // kommt nur nach Held
+        data[1] += 0;
+    if (b == ClickEncoder::Held)
+        data[1] = 0;
+
+    counter++;
+    if (counter > 1000)
+    {
+        counter = 0;
+        // der kram dauert ewig
+        tm1637.display(0, data[0] & 0x0f);
+        tm1637.display(1, data[1] & 0x0f);
+        tm1637.display(2, data[2] & 0x0f);
+        tm1637.display(3, data[3] & 0x0f);
+    }
+
+    digitalWrite(pinLatch, LOW);
+    digitalWrite(pinLatch, HIGH);
+    // data[2] = SPI.transfer(0xaa);
+    // data[3] = SPI.transfer(0x33);
+    digitalWrite(pinSCLK, LOW);
+    digitalWrite(pinSCLK, HIGH);
+    digitalWrite(pinSCLK, LOW);
+    digitalWrite(pinSCLK, HIGH);
+    digitalWrite(pinSCLK, LOW);
+    digitalWrite(pinSCLK, HIGH);
+    digitalWrite(pinSCLK, LOW);
+    digitalWrite(pinSCLK, HIGH);
+}
+
+void timerIsr()
+{
+    encoder.service();
+}
+
+void OnTinyWireSRequest()
+{
+    for (byte i = 0; i < NUM_BYTES; i++)
+    {
+        TinyWireS.write(data[i]);
+        data[i] += 1;
+    }
+    data[0] = 0;
     data[1] = 0;
-
-  counter++;
-  if (counter > 1000) {
-    counter = 0;
-    // der kram dauert ewig
-    tm1637.display(0, data[0] & 0x0f);
-    tm1637.display(1, data[1] & 0x0f);
-    tm1637.display(2, data[2] & 0x0f);
-    tm1637.display(3, data[3] & 0x0f);
-  }
-
-  data[2] = SPI.transfer(0x00);
-  data[3] = SPI.transfer(0xff);
-}
-
-void timerIsr() {
-  encoder.service();
-}
-
-void OnTinyWireSRequest() {
-  for (byte i = 0; i < NUM_BYTES; i++) {
-    TinyWireS.write(data[i]);
-    data[i] += 1;
-  }
-  data[0] = 0;
-  data[1] = 0;
-  data[2] = 0;
-  data[3] = 0;
+    data[2] = 0;
+    data[3] = 0;
 }
