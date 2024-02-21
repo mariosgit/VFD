e83bfa6e95cf329c24abdad1c080625c70568521
Mario Becker <mario.becker@augmentiv.com>
2024-02-21 15:21:56 +0100

    Progress with MN12832L...

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 
 /// wrapping the different code examples into main...
 /// choose one !
-#define RUN_MN12832L_TEST
+// #define RUN_MN12832L_GREY_TEST
+#define RUN_MN12832L_MONO_TEST
 // #define RUN_FREEDSPCONTROLLER
 // #define RUN_PANELDEMO
 
 /// do not touch anything below :)
 
-#ifdef RUN_MN12832L_TEST
-#include <examples/MN12832Ltest.h>
-MN12832Ltest test;
-#define SETUP test.setup();
-#define LOOP test.loop();
+#ifdef RUN_MN12832L_GREY_TEST
+#include <examples/MN12832grey.ino>
+#endif
+
+#ifdef RUN_MN12832L_MONO_TEST
+#include <examples/MN12832mono.ino>
 #endif
 
 #ifdef RUN_PANELDEMO
@@ -29,5 +31,7 @@ FreeDSPController fc;
 #define LOOP fc.loop();
 #endif
 
+#if !defined(RUN_MN12832L_MONO_TEST) && !defined(RUN_MN12832L_GREY_TEST)
 void setup() { SETUP }
 void loop() { LOOP }
+#endif
