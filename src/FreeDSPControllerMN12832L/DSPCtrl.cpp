c2f64031ba8dbdbeaad81733b867a20ae30e00af
Mario Becker <mario.becker@augmentiv.com>
2024-10-11 19:09:19 +0200

    Moved more stuff to SigmaDSP lib

--- src/FreeDSPControllerMN12832L/DSPCtrl.cpp
+++ src/FreeDSPControllerMN12832L/DSPCtrl.cpp
@@ -1,5 +1,3 @@
-#include <Wire.h>
-
 #include <mbLog.h>
 #include "DSPCtrl.h"
 
@@ -8,12 +6,8 @@
 #include "AMPx4-TDM-Test02/SigmaKram_IC_1_REG.h"
 
 
-DSPCtrl::DSPCtrl()
+DSPCtrl::DSPCtrl() : dsp()
 {
-    // DSP default adr
-    addr = 0x68 >> 1;
-
-
 }
 
 void DSPCtrl::readLevels()
@@ -28,69 +22,46 @@ void DSPCtrl::readLevels()
         const int16_t trapLevel3 = MOD_LVPOSTEQ_ALG0_SINGLEBANDLEVELDET2_VALUES;
         const int16_t trapLevel4 = MOD_LVDISTORTION_ALG0_SINGLEBANDLEVELDET1_VALUES;
 
-        uint8_t data[6 * 4];
-        // Write to data capture reg 2074 and 2075
-        const int16_t DC1 = 2074;
-        // const int16_t DC2 = 2075;
+
+        // We read 4x8 times
+        // toggling between reading volume and others
+        // could have used a second timer... :P
+
+        // 0 1 2 3 4 5 6 7    8 9 10 11 12 13 14 15   16 17 18 19 20 21 22 23    24..29..31
+        // |       |          |          |             |           |              |   |     read level meter
+        //   0       1          2           3             4           5            6   7    set EQ to preset dspReadCycle/4
+        //                                                                                  time for EQ to settle
+        //       |       |            |           |             |           |        |    | read EQ
 
         if (dspReadCycle % 4 == 0) // as long as we use only these 2, setup only once
         {
             // setup DC to read LevelMeters
-            Wire.beginTransmission(addr);
-            Wire.write(DC1 >> 8);          // address high byte
-            Wire.write(DC1 & 0xff);        // address low byte
-            Wire.write(trapLevel1 >> 8);   // address high byte
-            Wire.write(trapLevel1 & 0xff); // address low byte
-            Wire.write(trapLevel2 >> 8);   // address high byte
-            Wire.write(trapLevel2 & 0xff); // address low byte
-            WireEndTransmission();
+            dsp.setDataCapture(trapLevel1, trapLevel2);
         }
         else
         {
-            // setup DC to read Secondary LevelMeters
-            Wire.beginTransmission(addr);
-            Wire.write(DC1 >> 8);          // address high byte
-            Wire.write(DC1 & 0xff);        // address low byte
-            Wire.write(trapLevel3 >> 8);   // address high byte
-            Wire.write(trapLevel3 & 0xff); // address low byte
-            Wire.write(trapLevel4 >> 8);   // address high byte
-            Wire.write(trapLevel4 & 0xff); // address low byte
-            WireEndTransmission();
+            // setup DC to read Secondary LevelMeters (post EQ and Distortion)
+            dsp.setDataCapture(trapLevel3, trapLevel4);
 
-            // select the index of the postEQ filter 0-8 / flat,100,200,400,800,1600,3200,6400,12800
-            // kein block write, es knackt !!!
+            // select the index of the postEQ filter 0-8 / flat,100,200,400,800,1600,3200,6400,12800 Hz
+            // no block write, -> sound crackls !!! useing saveload
             if(dspReadCycle % 4 == 1)
             {
-                saveloadWrite(MOD_EQFILTERIDX_DCINPALG1_ADDR, dspReadCycle/4);
+                dspEnabled &= dsp.saveloadWrite(MOD_EQFILTERIDX_DCINPALG1_ADDR, dspReadCycle/4);
             }
         }
 
-        // send addr to read, no stop condition... then read...
-        Wire.beginTransmission(addr);
-        Wire.write(DC1 >> 8);                 // address high byte
-        Wire.write(DC1 & 0xff);               // address low byte
-        WireEndTransmission(false); // no stop before reading !
-        // LOG << "r2: " <<result << LOG.endl;
-
-        // read...
-        byte readlen = 6; // 2x3
-        Wire.requestFrom(addr, readlen);
-        int idx = 0;
-        while (Wire.available()) // slave may send less than requested
+        int32_t value1, value2;
+        dspEnabled &= dsp.readDataCapture(value1, value2);
+        // if(random(50) == 10) { LOG <<"DC:" <<value1 <<", " <<value2 <<LOG.endl; }
+        if (dspEnabled)
         {
-            data[idx++] = Wire.read(); // receive a byte as character
-            if (idx > readlen)
-                break;
-        }
+            // magick conversion to db values !? Where did this come from FreeDSP demo code ?
 
-        if (idx == 6)
-        {
-            // dcdone = true;
-            // 24bit 5.19 fixpoint value. The overall shift is needed for negative values.
-            int32_t value1 = (((int32_t)data[2] << 8) | ((int32_t)data[1] << 16) | ((int32_t)data[0] << 24)) >> 8;
-            int32_t value2 = (((int32_t)data[5] << 8) | ((int32_t)data[4] << 16) | ((int32_t)data[3] << 24)) >> 8;
+            // (1<<19) = 524.288
+            // 5242.880f = (1<<19) / 100
 
-            // 5.19 fixpoint to float.
+            // The captured data is in 5.19, 5.19 fixpoint to float.
             float realval1 = -1.0f * (100.0f - (float)value1 / 5242.880f); // x / (1 << 19)
             float realval2 = -1.0f * (100.0f - (float)value2 / 5242.880f); // x / (1 << 19)
 
@@ -98,6 +69,14 @@ void DSPCtrl::readLevels()
             {
                 levels.inL = realval1;
                 levels.inR = realval2;
+
+                // try clean conversion !? Still strange value range !
+                if(random(50) == 10)
+                {
+                    float from519 = dsp.from519(value1);
+                    float logval1 = -100*(1.0f-from519); //20 * logf(from519); // nee is schon log ?
+                    LOG <<"levels int:" <<LOG.dec <<value1 <<", db(old)" <<realval1 <<" float:" <<from519 <<" db(new):"<<logval1 <<LOG.dec <<LOG.endl;
+                }
             }
             else
             {
@@ -113,8 +92,7 @@ void DSPCtrl::readLevels()
             // LOG << "ac: ??? idx:" << idx << LOG.endl;
         }
 
-        dspReadCycle++; // next time read the others...
-
+        dspReadCycle++;
         if(dspReadCycle > 8*4)
         {
             dspReadCycle = 0;
@@ -128,153 +106,5 @@ void DSPCtrl::setVolume(int16_t volumeDB)
     float fraction = pow10f((float)volumeDB/20.0);
     uint32_t dspval = (uint32_t)(fraction * (float)0x0800000);
     LOG <<"Volume:" <<LOG.dec <<volumeDB <<" fract:" <<fraction <<" dsp:" <<LOG.hex <<dspval <<LOG.endl;
-    saveloadWrite(MOD_VOLUME_ALG0_TARGET_ADDR, dspval);
-
-}
-
-void DSPCtrl::saveloadWrite(uint16_t address, uint32_t value)
-{
-    // 1. write data to 0x0810 (5 bytes for whatever reason - SigmaStudio does not show the 5th !)
-    // 2. write target addr to 0x0815 - doc echt
-    //    repeat for up to 5 val/addr pairs in to 0x0811,12,13,14 and 16,17,18,19
-    // 3 write exec command to CoreRegister 0x081C < 0x0034 ? das default steht in so nem header.. 1C ? + IST bit 
-
-    if(!dspEnabled) return;
-
-    uint16_t coreregval = REG_COREREGISTER_IC_1_VALUE | R13_SAFELOAD_IC_1_MASK;
-
-    // LOG <<LOG.hex <<"saveload hex: " <<LOG.endl;
-    // LOG <<uint8_t(0x08);          // address high byte
-    // LOG <<uint8_t(0x10);        // address low byte
-    // LOG <<" ";
-    // LOG <<uint8_t(value >> 24);   // address high byte
-    // LOG <<uint8_t(value >> 16); // address low byte
-    // LOG <<uint8_t(value >> 8);   // address high byte
-    // LOG <<uint8_t(value); // address low byte
-    // LOG <<LOG.endl;
-    // LOG <<uint8_t(0x08);          // address high byte
-    // LOG <<uint8_t(0x15);        // address low byte
-    // LOG <<" ";
-    // LOG <<uint8_t(address >> 8);   // address high byte
-    // LOG <<uint8_t(address); // address low byte
-    // LOG <<LOG.endl;
-    // LOG <<uint8_t(REG_COREREGISTER_IC_1_ADDR >> 8);          // address high byte
-    // LOG <<uint8_t(REG_COREREGISTER_IC_1_ADDR);        // address low byte
-    // LOG <<" ";
-    // LOG <<uint8_t(coreregval >> 8);   // address high byte
-    // LOG <<uint8_t(coreregval); // address low byte
-    // LOG <<LOG.dec <<LOG.endl;
-
-    // return;
-
-    uint8_t result = 0;
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x10);        // address low byte
-    Wire.write(0);   // bits 39-32 ???
-    Wire.write(value >> 24);   // address high byte
-    Wire.write(value >> 16); // address low byte
-    Wire.write(value >> 8);   // address high byte
-    Wire.write(value & 0xff); // address low byte
-    WireEndTransmission();
-
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x15);        // address low byte
-    Wire.write(address >> 8);   // address high byte
-    Wire.write(address); // address low byte
-    WireEndTransmission();
-
-    // // also write "step" for slew volumes ??? neee brauch mer nich
-    // value = 0x00000800;
-    // Wire.beginTransmission(_addr);
-    // Wire.write(0x08);          // address high byte
-    // Wire.write(0x11);        // address low byte
-    // Wire.write(0);   // bits 39-32 ???
-    // Wire.write(value >> 24);   // address high byte
-    // Wire.write(value >> 16); // address low byte
-    // Wire.write(value >> 8);   // address high byte
-    // Wire.write(value & 0xff); // address low byte
-    //  WireEndTransmission();
-    // 
-    // Wire.beginTransmission(_addr);
-    // Wire.write(0x08);          // address high byte
-    // Wire.write(0x16);        // address low byte
-    // Wire.write(MOD_VOLUME_ALG0_STEP_ADDR >> 8);   // address high byte
-    // Wire.write(MOD_VOLUME_ALG0_STEP_ADDR); // address low byte
-    // WireEndTransmission();
-    // 
-
-    Wire.beginTransmission(addr);
-    Wire.write(REG_COREREGISTER_IC_1_ADDR >> 8);          // address high byte
-    Wire.write(REG_COREREGISTER_IC_1_ADDR);        // address low byte
-    Wire.write(coreregval >> 8);   // address high byte
-    Wire.write(coreregval); // address low byte
-    WireEndTransmission();
-}
-
-void DSPCtrl::saveloadWrite(uint16_t address1, uint32_t value1, uint16_t address2, uint32_t value2)
-{
-    // 1. write data to 0x0810 (5 bytes for whatever reason - SigmaStudio does not show the 5th !)
-    // 2. write target addr to 0x0815 - doc echt
-    //    repeat for up to 5 val/addr pairs in to 0x0811,12,13,14 and 16,17,18,19
-    // 3 write exec command to CoreRegister 0x081C < 0x0034 ? das default steht in so nem header.. 1C ? + IST bit 
-
-    if(!dspEnabled) return;
-
-    uint16_t coreregval = REG_COREREGISTER_IC_1_VALUE | R13_SAFELOAD_IC_1_MASK;
-
-    uint8_t result = 0;
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x10);        // address low byte
-    Wire.write(0);   // bits 39-32 ???
-    Wire.write(value1 >> 24);   // address high byte
-    Wire.write(value1 >> 16); // address low byte
-    Wire.write(value1 >> 8);   // address high byte
-    Wire.write(value1 & 0xff); // address low byte
-    WireEndTransmission();
-
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x15);        // address low byte
-    Wire.write(address1 >> 8);   // address high byte
-    Wire.write(address1); // address low byte
-    WireEndTransmission();
-
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x11);        // address low byte
-    Wire.write(0);   // bits 39-32 ???
-    Wire.write(value2 >> 24);   // address high byte
-    Wire.write(value2 >> 16); // address low byte
-    Wire.write(value2 >> 8);   // address high byte
-    Wire.write(value2 & 0xff); // address low byte
-    WireEndTransmission();
-
-    Wire.beginTransmission(addr);
-    Wire.write(0x08);          // address high byte
-    Wire.write(0x16);        // address low byte
-    Wire.write(address2 >> 8);   // address high byte
-    Wire.write(address2); // address low byte
-    WireEndTransmission();
-
-    Wire.beginTransmission(addr);
-    Wire.write(REG_COREREGISTER_IC_1_ADDR >> 8);          // address high byte
-    Wire.write(REG_COREREGISTER_IC_1_ADDR);        // address low byte
-    Wire.write(coreregval >> 8);   // address high byte
-    Wire.write(coreregval); // address low byte
-    WireEndTransmission();
-}
-
-
-/// @brief Finish transmission, check result, stops DSP activity on error
-void DSPCtrl::WireEndTransmission(boolean sendStop)
-{
-    uint8_t result = Wire.endTransmission(sendStop);
-    if (result != 0)
-    {
-        LOG << "saveloadWrite:i2c error " << result << LOG.endl;
-        dspEnabled = false;
-    }
+    dspEnabled &= dsp.saveloadWrite(MOD_VOLUME_ALG0_TARGET_ADDR, dspval);
 }
--- src/FreeDSPControllerMN12832L/DSPCtrl.h
+++ src/FreeDSPControllerMN12832L/DSPCtrl.h
@@ -2,20 +2,14 @@
 
 #include <Arduino.h>
 
+#include <SigmaDSP.h>
+
 class DSPCtrl {
   public:
     DSPCtrl();
 
     void readLevels();
     void setVolume(int16_t volumeDB);
-    void saveloadWrite(uint16_t address, uint32_t value);
-    void saveloadWrite(uint16_t address1, uint32_t value1,
-                       uint16_t address2, uint32_t value2);
-
-    inline uint32_t floatTo523(float val) { return val * (float)0x0800000; }
-    
-    /// @brief DSP I2C address
-    uint8_t addr = 0;
 
     /// @brief If disabled, I2C comm stops, can connect sigmastudio then.
     boolean dspEnabled = false;
@@ -29,7 +23,5 @@ class DSPCtrl {
         float postEQ[10];
     } levels = {-99,-99,-99,{-99,-99,-99,-99,-99,-99,-99,-99,-99,-99}};
 
-  private:
-    void WireEndTransmission(boolean sendStop=true);
-
+    SigmaDSP dsp;
 };
